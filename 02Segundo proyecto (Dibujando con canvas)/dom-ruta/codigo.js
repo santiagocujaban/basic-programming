875a317948512229bd9434d27badefe5abc43ed1
Santiago Cujaban <santiero12@gmail.com>
2018-12-26 12:09:40 -0500

    Se ha documentado archivo codigo.js

--- 02Segundo proyecto (Dibujando con canvas)/dom-ruta/codigo.js
+++ 02Segundo proyecto (Dibujando con canvas)/dom-ruta/codigo.js
@@ -1,3 +1,3 @@
-var ruta = window.location;
-console.log("Mensaje secreto :)");
-document.write("Estás en: " + ruta);
+var ruta = window.location; // window.location es la URL de la ventana en que estamos
+console.log("Mensaje secreto :)"); // Mensaje que pasa por consola
+document.write("Estás en: " + ruta); // Se escibe en el documento el string indicado y la ruta de la ventana abierta
