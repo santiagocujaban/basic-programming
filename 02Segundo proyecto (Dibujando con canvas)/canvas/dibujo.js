f7ea504f9be49eec7791169b0201485333061039
Santiago Cujaban <santiero12@gmail.com>
2018-12-26 12:00:55 -0500

    Se ha documentado archivo dibujo.js

--- 02Segundo proyecto (Dibujando con canvas)/canvas/dibujo.js
+++ 02Segundo proyecto (Dibujando con canvas)/canvas/dibujo.js
@@ -14,32 +14,32 @@ function clic()
 
 function pinturilloxd(colorsito,xi,yi,xf,yf)
 {
-  lienzo.beginPath();
-  lienzo.strokeStyle = colorsito;
-  lienzo.moveTo(xi,yi);
-  lienzo.lineTo(xf,yf);
-  lienzo.stroke();
-  lienzo.closePath();
+  lienzo.beginPath(); // Traer pincel
+  lienzo.strokeStyle = colorsito; // Color del trazo
+  lienzo.moveTo(xi,yi); // Punto de partida del pincel
+  lienzo.lineTo(xf,yf); // Punto final del pincel
+  lienzo.stroke(); // Dibuja aquel trazo definido anteriormente
+  lienzo.closePath(); // "Cerrar" pincel
 }
 
 function dibujoPorClic()
 {
-  var xxx = parseInt(texto.value);
+  var xxx = parseInt(texto.value); // Se pasa a entero el dato ingresado en texto
   var lineas = xxx;
   var l = 0;
   var xi, yi, xf, yf, rxf, ryf;
   var color = "grey";
   var espacio = ancho / lineas;
 
-  do{
+  do{            // Ciclo do{...}while(...) (algo así como "hacer mientras")
     l++;
     xi = espacio * l;
     yf = espacio * (l + 1);
     pinturilloxd(color,xi,1,299,yf);
     console.log("linea " + l);
-  }while(l < lineas);
+  }while(l < lineas); 
 
-  for(l = 0; l < lineas; l++)
+  for(l = 0; l < lineas; l++)  // Se encarga de dibujar las lineas de la figura final...
   {
     xi = espacio * l;
     yi = espacio * (l + 1);
@@ -50,7 +50,7 @@ function dibujoPorClic()
     pinturilloxd(color,1,yi,xf,299);
     pinturilloxd(color,xi,299,299,ryf);
     pinturilloxd(color,1,yi,rxf,1);
-    console.log("linea " + l);
+    console.log("linea " + l); // Mensaje de comprobación
   }
 
   pinturilloxd(color,1,1,1,299);
